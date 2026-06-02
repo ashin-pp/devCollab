15cc4785473423e110a3472a4ddaa145b48098f7
ashin-pp <ashinpp4132@gmail.com>
2026-06-02 14:10:29 +0530

    testing

--- server/src/app.ts
+++ server/src/app.ts
@@ -13,7 +13,7 @@ const app = express();
 app.use(cookieParser());
 
 app.use(cors({
-    origin: [process.env.CLIENT_URL || "http://localhost:5173", "http://localhost:5174"],
+    origin: [process.env.CLIENT_URL || "http://localhost:5173"],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
 }));
